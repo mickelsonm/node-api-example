513216c1453694d1cfb053b1482297b315cde713
mickelsonm <mickelmr@gmail.com>
2017-06-11 23:21:21 -0500

    adds an external call to another system (good ole google)

--- routes/users/index.js
+++ routes/users/index.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var _request = require('request');
 
 router.route("/")
   .get(function(req,res){
     console.log('called get user');
     res.json({ response: 'get user' });
+  });
+
+router.route("/external")
+  .get(function(req,res){
+    _request({
+      uri: 'http://google.com',
+      method: 'get'
+    }, function(err,resp,body){
+      if(err){
+        res.json({ error: 'failed to call google'});
+      } else{
+        res.json({ response: resp.body });
+      }
+    });
   });
 
 module.exports = router;
