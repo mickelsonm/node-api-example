30ea43ff13ea09d7425e52d310d795eb0149bc19
mickelsonm <mickelmr@gmail.com>
2017-06-11 23:30:02 -0500

    adds some commenting and minor adjustments

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var app = express();
 
 var bodyParser = require('body-parser');
 
+//global settings, middleware, etc
 app.use(bodyParser.urlencoded({
   extended: true,
   limit: '5m'
 }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 3000;
-
+//sets up global routing
 var router = express.Router();
 
 router.get('/', function(req,res){
@@ -19,11 +19,16 @@ router.get('/', function(req,res){
   });
 });
 
+//sets up routes within our application
 var users = require("./routes/users");
 app.use('/users', users);
 
+//hooks up our router to this application
 app.use('/', router);
 
+//our app eagerly listens on its default port or port set on the
+//appropriate environment variable
+var port = process.env.PORT || 3000;
 app.listen(port, function(){
   console.log('listening on', port);
 });
--- routes/users/index.js
+++ routes/users/index.js
@@ -4,15 +4,18 @@ var _request = require('request');
 
 router.route("/")
   .get(function(req,res){
+    //we use console.log() just to show on our api that the route got called!
     console.log('called get user');
     res.json({ response: 'get user' });
   });
 
 router.route("/external")
   .get(function(req,res){
+    //this is just an example showing how we could call another source (ex. api)
     _request({
       uri: 'http://google.com',
       method: 'get'
+      //,json: resp.body -- used to pass json data using post, put, etc
     }, function(err,resp,body){
       if(err){
         res.json({ error: 'failed to call google'});
